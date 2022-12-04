98abd7d099b016d256dffb37518cb63778ea6d6b
Debanjan Mitra <debanjan.mitra@ca.ey.com>
2022-12-04 17:52:46 +0000

    Added network module v3

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/ey-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "debanjan-vnet"
+  resource_group_name = "debanjan-workshop"
 }
